3cd807e2b1083ac5ed4e98cc286b8c05c8cb155e
Romjkensky <talaevroma@gmail.com>
2023-07-25 21:43:46 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5.5(2)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._5_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string userText = Console.ReadLine();
+            string reversed = Reverse(userText);
+            Console.WriteLine("Переставленные слова: " + reversed);
+        }
+
+        static string[] SplitText(string text)
+        {
+            char[] delimiter = { ' ' };
+            string[] words = text.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+
+        static string Reverse(string text)
+        {
+            string[] words = SplitText(text);
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+    }
+}
---
+++ HomeWork5.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string userText = Console.ReadLine(); 
+            string[] words = SplitText(userText); 
+            Console.WriteLine("Слова в предложении:");
+            PrintWords(words);
+        }
+
+        static string[] SplitText(string text)
+        {
+            char[] delimiter = { ' ' };
+            string[] words = text.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+
+        static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
